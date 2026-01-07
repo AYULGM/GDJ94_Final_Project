f608ce2fb5e0b20597020a006e6fc496efd81923
kdhcode <jwah3@naver.com>
2026-01-07 09:16:23 +0900

    폼 데이터 연결중
    보고서양식 수정을 위한 DTO 추가 및 연결

--- src/main/java/com/health/app/approval/ApprovalController.java
+++ src/main/java/com/health/app/approval/ApprovalController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import com.health.app.approval.dto.ApprovalProductDto;
+import com.health.app.approval.ApprovalProductDTO;
 import com.health.app.security.model.LoginUser;
 
 import lombok.RequiredArgsConstructor;
